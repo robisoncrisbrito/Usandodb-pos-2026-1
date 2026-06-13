125892de6df28c4c19a9514a69f252893c626619
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-06-13 08:58:17 -0300

    projeto com código kotlin e interface xml, porém, sem usar sqlite.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 android {
     namespace = "br.edu.utfpr.usandodb"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
--- app/src/main/java/br/edu/utfpr/usandodb/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandodb/MainActivity.kt
@@ -1,20 +1,34 @@
 package br.edu.utfpr.usandodb
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.usandodb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btIncluir.setOnClickListener {  }
+        binding.btAlterar.setOnClickListener {  }
+        binding.btExcluir.setOnClickListener {  }
+        binding.btPesquisar.setOnClickListener {  }
+        binding.btListar.setOnClickListener {  }
+
     }
 }
