994a9ef638ea3eda2d1d95c5bc63eb89d226b2cb
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-06-13 14:44:10 -0300

    projeto com uma lista personalizada, já com botão e com tratamento de clique

--- app/src/main/java/br/edu/utfpr/usandodb/adapter/ElementoListaAdapter.kt
+++ app/src/main/java/br/edu/utfpr/usandodb/adapter/ElementoListaAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import br.edu.utfpr.usandodb.R
 import br.edu.utfpr.usandodb.entity.Cadastro
 
@@ -41,6 +42,7 @@ class ElementoListaAdapter(val context: Context, val registros: MutableList<Cada
         val tvNomeElementoLista: TextView = v.findViewById(R.id.tvNomeElementoLista)
         val tvTelefoneElementoLista: TextView = v.findViewById(R.id.tvTelefoneElementoLista)
         val imageView: ImageView = v.findViewById(R.id.imageView)
+        val btEditarElementoLista: ImageView = v.findViewById(R.id.btEditarElementoLista)
 
         tvNomeElementoLista.text = registros[pos].nome
         tvTelefoneElementoLista.text = registros[pos].telefone
@@ -49,6 +51,14 @@ class ElementoListaAdapter(val context: Context, val registros: MutableList<Cada
             imageView.setImageResource(android.R.drawable.star_big_on)
         } else {
             imageView.setImageResource(android.R.drawable.star_big_off)
+        }
+
+        btEditarElementoLista.setOnClickListener {
+            Toast.makeText(
+                context,
+                "clique em $pos",
+                Toast.LENGTH_LONG
+            ).show()
         }
 
         return v
